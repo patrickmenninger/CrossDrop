88d19ef6111a275b0b7367d2a3b92a55351b3e05
Patrick Menninger <ptmennin@ncsu.edu>
2025-11-18 18:45:59 -0500

    Turned off turn server

--- server/server.ts
+++ server/server.ts
@@ -44,26 +44,26 @@ app.get("/api/turn", async (req: express.Request, res: express.Response) => {
         {
             urls: "stun:stun.l.google.com:19302",
         },
-        {
-            urls: "turn:standard.relay.metered.ca:80",
-            username: process.env.TURN_USERNAME,
-            credential: process.env.TURN_PASSWORD,
-        },
-        {
-            urls: "turn:standard.relay.metered.ca:80?transport=tcp",
-            username: process.env.TURN_USERNAME,
-            credential: process.env.TURN_PASSWORD,
-        },
-        {
-            urls: "turn:standard.relay.metered.ca:443",
-            username: process.env.TURN_USERNAME,
-            credential: process.env.TURN_PASSWORD,
-        },
-        {
-            urls: "turns:standard.relay.metered.ca:443?transport=tcp",
-            username: process.env.TURN_USERNAME,
-            credential: process.env.TURN_PASSWORD,
-        },
+        // {
+        //     urls: "turn:standard.relay.metered.ca:80",
+        //     username: process.env.TURN_USERNAME,
+        //     credential: process.env.TURN_PASSWORD,
+        // },
+        // {
+        //     urls: "turn:standard.relay.metered.ca:80?transport=tcp",
+        //     username: process.env.TURN_USERNAME,
+        //     credential: process.env.TURN_PASSWORD,
+        // },
+        // {
+        //     urls: "turn:standard.relay.metered.ca:443",
+        //     username: process.env.TURN_USERNAME,
+        //     credential: process.env.TURN_PASSWORD,
+        // },
+        // {
+        //     urls: "turns:standard.relay.metered.ca:443?transport=tcp",
+        //     username: process.env.TURN_USERNAME,
+        //     credential: process.env.TURN_PASSWORD,
+        // },
     ]
 
     res.status(200).json(iceServers);
